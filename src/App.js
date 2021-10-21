0c65b6cc568d2f41375f6d31990a4f8753ab36dc
Lucianofc138 <lucianofc138@gmail.com>
2021-10-21 00:51:47 -0300

    fix mqtt connection between local broker with websocket and pub

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import {useState} from "react";
+import { useState } from "react";
+import {pubHi} from "./helpers";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [values, setValues] = useState([]);
-  
+
   const handleClick = () => {
-    setCounter((prevState) => prevState+1);
+    setCounter((prevState) => prevState + 1);
     setValues((prevState) => prevState.concat(counter));
+
   };
-  
+
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Hello Luciano!</h1>
       <button onClick={handleClick}> press this!</button>
       <div>
